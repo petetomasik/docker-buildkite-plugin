f0cb833e1d4c1ab118abf67189800185cb192990
Pete Tomasik <senate6268@gmail.com>
2024-11-06 11:11:16 -0500

    local vars

--- commands/run.sh
+++ commands/run.sh
@@ -350,7 +350,7 @@ if [[ "${BUILDKITE_PLUGIN_DOCKER_ALWAYS_PULL:-false}" =~ ^(true|on|1)$ ]] ; then
   echo "--- :docker: Pulling ${image}"
   if ! retry "${BUILDKITE_PLUGIN_DOCKER_PULL_RETRIES:-3}" \
        docker pull "${image}" ; then
-    retry_exit_status="$?"
+    local retry_exit_status="$?"
     echo -e "!!! :docker: Pull failed: retry_exit_status=$retry_exit_status"
     exit "$retry_exit_status"
   fi
